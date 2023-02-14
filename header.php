bc1605a90c20149e9104d454cfc7e1b6b81b05ac
tarikjessoreit <tarikjessoreit@gmail.com>
2023-02-14 14:03:52 +0600

    ready index

---
+++ header.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"/>
+</head>
+
+<body>
+    <!-- Header Navbar -->
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">ABlood</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#headernavebar"
+                aria-controls="headernavebar" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="headernavebar">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                </ul>
+                <!-- <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form> -->
+
+                <!-- header user section -->
+                <div class="dropdown">
+                    <button class="btn btn-light dropdown-toggle px-4 me-4" type="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        <img src="assets/images/default-profile-avater.png" width="30" alt="">
+                        Mr. Jamse
+                    </button>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Profile</a></li>
+                        <li><a class="dropdown-item" href="#">Settings</a></li>
+                        <li><a class="dropdown-item" href="#">Logout</a></li>
+                    </ul>
+                </div><!-- end header user section -->
+
+            </div>
+        </div>
+    </nav><!--end Navbar-->
---
+++ index.php
@@ -0,0 +1,66 @@
+<?php require('header.php');?>
+    <!-- main -->
+    <div class="container mian mt-4">
+        <div class="row">
+            <div class="col-8 m-auto">
+                <div class="card">
+                    <form action="">
+                        <div class="input-group">
+                            <select class="form-select" id="inputGroupSelect04"
+                                aria-label="Example select with button addon">
+                                <option selected>Select Blood Group</option>
+                                <option value="A+">A+</option>
+                                <option value="A-">A-</option>
+                                <option value="B+">B+</option>
+                                <option value="B-">B-</option>
+                                <option value="O+">O+</option>
+                                <option value="O-">O-</option>
+                                <option value="AB+">AB+</option>
+                                <option value="AB-">AB-</option>
+                            </select>
+                            <button class="btn btn-outline-secondary" type="submit">Search</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+        <!-- search result -->
+        <div class="row mt-4">
+            <div class="col">
+                <h2 class="text-center mb-4">Search Result</h2>
+                <!-- Item -->
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                        <div class="col-md-1 mb-0">
+                        <img src="assets/images/default-search-avater.png" width="50" alt="">
+                            </div>
+
+                            <div class="col-md-7 mb-0">
+                                <h4>Mr. Hablu (O+)</h4>
+                                <p>Jessore, Khulna, Bangladesh</p>
+                            </div>
+                            
+                            <div class="col-md-4 text-end">
+                            <div class="input-group">
+                                <input type="text" class="form-control" value="017777777777">
+                                <button type="button" class="btn btn-sm px-3 btn-outline-primary"><i class="fa fa-eye"></i></button>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                </div><!-- endItem -->
+
+            </div>
+        </div>
+    </div>
+
+
+
+
+    <footer class="text-center border-top mt-4 p-2">&copy; by <?php echo date('Y');?></footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
